c3bb15642f6b41f8845c468cb363715bdff6a653
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2023-05-25 10:36:38 +0200

    add handle method to either

--- src/either.ts
+++ src/either.ts
@@ -1,7 +1,9 @@
 abstract class IEither<L, R> {
 
-  abstract map<S>(f: ((r: R) => S)): IEither<L, S>;
+  abstract map<S>(f: (r: R) => S): IEither<L, S>;
 
+  // noinspection JSUnusedGlobalSymbols
+  abstract handle(f: (r: R) => void, g: (l: L) => void): void;
 }
 
 export class MyLeft<L> extends IEither<L, never> {
@@ -12,6 +14,10 @@ export class MyLeft<L> extends IEither<L, never> {
   override map<S>(): IEither<L, S> {
     return this;
   }
+
+  override handle(f: (r: never) => void, g: (l: L) => void) {
+    g(this.value);
+  }
 }
 
 export class MyRight<R> extends IEither<never, R> {
@@ -21,6 +27,10 @@ export class MyRight<R> extends IEither<never, R> {
 
   override map<S>(f: (r: R) => S): IEither<never, S> {
     return new MyRight(f(this.value));
+  }
+
+  override handle(f: (r: R) => void) {
+    f(this.value);
   }
 }
 
